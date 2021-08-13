14f1c01efd506e4dafd9499f7c64c96cbc7087c1
GennadiiRoud <gennady@roudstudio.com>
2021-08-13 13:40:49 +0300

    footer changes

--- footer.php
+++ footer.php
@@ -14,6 +14,7 @@
 <footer class="footer">
   <div class="footer__center center">
     <div class="footer__nav">
+      <div class="footer__inner">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint maiores quidem repellat omnis. Eligendi ea in debitis magnam earum, esse distinctio officia. Est aperiam, repellat veniam asperiores ut quisquam dicta necessitatibus, maiores temporibus mollitia iste, praesentium similique omnis aspernatur perspiciatis doloremque, quidem amet magnam reiciendis blanditiis voluptate quae. Ut, cum est natus? Veritatis, at deserunt itaque commodi minus repellendus optio nobis, reprehenderit iusto quam cumque quidem fugit quibusdam sed fuga dolore eos voluptates. Nostrum, explicabo, eaque? At, provident cumque totam quis tempora asperiores aperiam consequatur nobis dolor corporis unde eum iure nulla obcaecati facilis velit quaerat quos excepturi ipsam.</div>
       <?php
       wp_nav_menu(
           array(
