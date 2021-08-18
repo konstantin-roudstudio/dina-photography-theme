849b43bdd0649637a876b44a334789c2b7f8f47e
konstantin-roudstudio <konstantin@roudstudio.com>
2021-08-18 11:42:46 +0300

    hero-info preparations

--- flexible.php
+++ flexible.php
@@ -6,14 +6,16 @@
 get_header();
 ?>
     <main id="main" class="main">
+
+
         <?php
+
+//        TEMP!
+        get_template_part('template-parts/rows/hero-info-section');
+
         if (have_rows('page_builder')):
             while (have_rows('page_builder')): the_row();
-                if (get_row_layout() == 'hero_section'): get_template_part('template-parts/rows/hero-section', 'section');
-                elseif (get_row_layout() == 'about_section'): get_template_part('template-parts/rows/about-section', 'section');
-                elseif (get_row_layout() == 'facts_section'): get_template_part('template-parts/rows/facts-section', 'section');
-                elseif (get_row_layout() == 'services_section'): get_template_part('template-parts/rows/services-section', 'section');
-                elseif (get_row_layout() == 'testimonials_section'): get_template_part('template-parts/rows/testimonials-section', 'section');
+                if (get_row_layout() == 'hero_info_section'): get_template_part('template-parts/rows/hero-info-section', 'section');
                 endif;
             endwhile;
         endif;
---
+++ template-parts/rows/hero-info-section.php
@@ -0,0 +1,17 @@
+<?php
+$title = get_sub_field('about_title');
+$image = get_sub_field('about_image');
+
+?>
+
+<section class="hero-info">
+  <div class="hero-info__bg hero-info__bg--desktop"></div>
+
+  <div class="hero-info__bg hero-info__bg--mobile"></div>
+
+  <div class="center hero-info__center">
+    <h1 class="hero-info__title title">wedding photographer in south of france</h1>
+    <a href="#" class="btn btn--solid">Explore Portfolio</a>
+  </div>
+</section>
+
