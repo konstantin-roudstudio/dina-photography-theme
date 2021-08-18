bcbb8b5ea9278695aec27dda0a0dc5de78656b4c
GennadiiRoud <72335453+GennadiiRoud@users.noreply.github.com>
2021-08-18 13:21:22 +0300

    hero-info changes

--- template-parts/rows/hero-info-section.php
+++ template-parts/rows/hero-info-section.php
@@ -5,13 +5,16 @@ $image = get_sub_field('about_image');
 ?>
 
 <section class="hero-info">
-  <div class="hero-info__bg hero-info__bg--desktop"></div>
+	<div class="hero-info__bg hero-info__bg--desktop">
+		<!-- <img src="<?php echo get_template_directory_uri(); ?>/images/hero-info-bg-desktop.jpg" alt=""> -->
+	</div>
 
-  <div class="hero-info__bg hero-info__bg--mobile"></div>
+	<div class="hero-info__bg hero-info__bg--mobile">
+		<!-- <img src="<?php echo get_template_directory_uri(); ?>/images/hero-info-bg-mobile.jpg" alt=""> -->
+	</div>
 
-  <div class="center hero-info__center">
-    <h1 class="hero-info__title title">wedding photographer in south of france</h1>
-    <a href="#" class="btn btn--solid">Explore Portfolio</a>
-  </div>
+	<div class="center hero-info__center">
+		<h1 class="hero-info__title title">wedding photographer in south of france</h1>
+		<a href="#" class="hero-info__btn btn btn--solid">Explore Portfolio</a>
+	</div>
 </section>
-
