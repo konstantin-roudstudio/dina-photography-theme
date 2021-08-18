b31aa939eda4ac947b7a3679cc97489cb41261ae
GennadiiRoud <72335453+GennadiiRoud@users.noreply.github.com>
2021-08-18 15:16:46 +0300

    hero-reviews changes

--- template-parts/rows/hero-reviews-section.php
+++ template-parts/rows/hero-reviews-section.php
@@ -11,7 +11,11 @@ $image = get_sub_field('about_image');
 
   <div class="center hero-reviews__center">
     <h1 class="hero-reviews__title title">Kind Words from my Brides & Grooms</h1>
-    <div class="hero-reviews__text">Thanks to everyone for your feedback about my wedding or pre-wedding photography. It means a lot to me that you love the images and having a great time with me as your photographer. </div>
+    <div class="hero-reviews__text">Thanks to everyone for your feedback about my wedding or pre-wedding photography. It means a lot to me that you love the images and having a great time with me as your photographer. 
+    <svg class="hero-reviews__arrow">
+      <path class="hero-reviews__arrow-path" d="M4 0.5V0H3V0.5H4ZM3.5 48.5L6.38675 43.5H0.613249L3.5 48.5ZM3 0.5V44H4V0.5H3Z" />
+    </svg>
+    </div>
   </div>
 </section>
 
