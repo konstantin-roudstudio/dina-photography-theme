b1a7212a4a333d2a5c27e2a360d3101dbbff0f1b
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-08-19 23:14:35 +0300

    add promo--text-right modifier

--- template-parts/rows/promo-section.php
+++ template-parts/rows/promo-section.php
@@ -1,9 +1,10 @@
 <section class="promo">
   <div class="promo__bg"></div>
   <div class="promo__overlay"></div>
-  <div class="promo__center center"></div>
+  <div class="promo__center center">
     <div class="promo__content">
       <h1 class="promo__title title">It’s my love for travelling and romance that set my destiny as a destination wedding photographer.</h1>
       <div class="promo__text">My ability to listen, form quick resolutions to problems, and strong-willed character make me stand out in the photography world.</div>
     </div>
+  </div>   
 </section>
