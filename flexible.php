3fb4756a551270bcc186f6d7e4abc7ad4feee418
konstantin-roudstudio <konstantin@roudstudio.com>
2021-08-19 09:58:44 +0300

    flexible update

--- flexible.php
+++ flexible.php
@@ -12,6 +12,7 @@ get_header();
         if (have_rows('page_builder')):
             while (have_rows('page_builder')): the_row();
                 if (get_row_layout() == 'hero_info_section'): get_template_part('template-parts/rows/hero-info-section', 'section');
+                elseif (get_row_layout() == 'hero_reviews_section'): get_template_part('template-parts/rows/hero-reviews-section', 'section');
                 endif;
             endwhile;
         endif;
