e91b306b5866e6756f554abd69cc30bca7f851f8
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-08-19 17:59:59 +0300

    add feedback section

--- template-parts/rows/feedback-section.php
+++ template-parts/rows/feedback-section.php
@@ -3,6 +3,21 @@
 ?>
 
 <section class="feedback">
-
+  <div class="feedback__bg"></div>
+  <div class="feedback__content-bg"></div>
+  <div class="feedback__center center">
+    <div class="feedback__content">
+      
+        <div class="feedback__img">
+          <img src="<?php echo get_template_directory_uri(); ?> /images/feedback__quote.png" alt="">
+        </div>
+        <blockquote>
+          <p>We were looking for a photographer that could deliver personal photographies with the best quality of our wedding day and we’re happy to say that Dina Deykun exceeded our expectations. She had a great sense of making our pictures  unique and improvised some amazing shots that we are now lucky to have for the rest of our lives.</p>
+          <p>We really recommend Dina to everyone who wants beautiful and unique pictures taken of their special day. Thank you Dina!</p>
+        </blockquote>
+        <div class="feedback__author">- Frej & Chinami</div> 
+      
+    </div>
+  </div>
 </section>
 
