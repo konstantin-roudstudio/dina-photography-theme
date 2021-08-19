451b6ad6de75df583a17f58ff52e572de53210cf
konstantin-roudstudio <konstantin@roudstudio.com>
2021-08-19 10:17:44 +0300

    fields connected

--- template-parts/rows/hero-reviews-section.php
+++ template-parts/rows/hero-reviews-section.php
@@ -1,17 +1,39 @@
 <?php
-$title = get_sub_field('about_title');
-$image = get_sub_field('about_image');
+$title = get_sub_field('title');
+$text = get_sub_field('text');
+$bg_id = get_sub_field('image');
+$bg_mob_id = get_sub_field('image_mobile');
+$link = get_sub_field('link');
+
+$bg_img = $bg_id ? wp_get_attachment_image($bg_id, 'full') : null;
+$bg_mob_img = $bg_mob_id ? wp_get_attachment_image($bg_mob_id, 'full') : null;
 
 ?>
 
 <section class="hero-reviews">
-  <div class="hero-reviews__bg hero-reviews__bg--desktop"></div>
+  <?php if ($bg_img): ?>
+    <div class="hero-reviews__bg<?php if ($bg_img && $bg_mob_img) echo ' hero-reviews__bg--desktop'; ?>">
+      <?= $bg_img ?>
+    </div>
+  <?php endif; ?>
 
-  <div class="hero-reviews__bg hero-reviews__bg--mobile"></div>
+  <?php if ($bg_mob_img): ?>
+    <div class="hero-reviews__bg<?php if ($bg_img && $bg_mob_img) echo ' hero-reviews__bg--mobile'; ?>">
+      <?= $bg_mob_img ?>
+    </div>
+  <?php endif; ?>
 
   <div class="center hero-reviews__center">
-    <h1 class="hero-reviews__title title">Kind Words from my Brides & Grooms</h1>
-    <div class="hero-reviews__text">Thanks to everyone for your feedback about my wedding or pre-wedding photography. It means a lot to me that you love the images and having a great time with me as your photographer. </div>
+    <?php if ($title) : ?>
+      <h1 class="hero-reviews__title title">Kind Words from my Brides & Grooms</h1>
+    <?php endif; ?>
+
+    <?php if ($text): ?>
+      <div class="hero-reviews__text">Thanks to everyone for your feedback about my wedding or pre-wedding photography. It means a lot to me that you love the images and having a great time with me as your photographer. </div>
+    <?php endif; ?>
+
+    <?php if ($link): ?>
+      <a href="<?= esc_url($link); ?>" class="hero-reviews__arrow"></a>
+    <?php endif; ?>
   </div>
 </section>
-
