06c66979c72f1f46c0f8135f01f69386d0bba154
GennadiiRoud <72335453+GennadiiRoud@users.noreply.github.com>
2021-08-20 14:24:22 +0300

    add changes

--- assets_src/js/modules/hero-slider.js
+++ assets_src/js/modules/hero-slider.js
@@ -1,13 +1,31 @@
-import Swiper, {Navigation} from 'swiper';
+import Swiper, { Navigation } from 'swiper';
 
 export default function heroSlider($) {
-    Swiper.use([Navigation]);
+  const hero = document.querySelectorAll('.hero-slider'); {
+    for (const block of hero) {
+      let slide = block.querySelectorAll('.swiper-slide');
+      let sliderNumCurrent = block.querySelector('.current');
+      let sliderNumTotal = block.querySelector('.total');
+    }
+  }
+  Swiper.use([Navigation]);
 
-    const swiper = new Swiper('.hero-slider .swiper-container', {
-        slidesPerView: 1,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        }
-    });
+  const swiper = new Swiper('.hero-slider .swiper-container', {
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    on: {
+      init: function(sw) {
+        $('.total').text($('.swiper-slide:not(.swiper-slide-duplicate)').length)
+        $('.current').text(sw.realIndex + 1)
+      },
+      slideChange: function(sw) {
+        $('.current').text(sw.realIndex + 1)
+      },
+    },
+  });
+
+
 }
