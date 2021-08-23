d412387231abfc991a0a1db7a586a6e455433860
GennadiiRoud <72335453+GennadiiRoud@users.noreply.github.com>
2021-08-23 15:03:54 +0300

    text-images cahnges

--- template-parts/rows/text-images-section.php
+++ template-parts/rows/text-images-section.php
@@ -1,7 +1,37 @@
 <?php
 ?>
 
-<section class="text-images">
-TEXT IMAGES
+<section class="text-images text-images--cream">
+  <div class="center">
+    <div class="text-images__inner">
+      <div class="text-images__content">
+      <h2 class="text-images__title">A story of a girl with passion for Photography and Traveling.</h2>
+      <div class="text-images__img-wrapper mob">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/text-images-mob.jpg" alt="" class="text-images__img">
+      </div>
+      <div class="text-images__text">
+        <p>Before becoming a photographer, I was working in the corporate space, specifically in the sales sector. While I was successful for many years in this work, I wasn’t happy and eventually found the courage to make a change in my professional life.</p>
+        <p>I’d always had an interest in photography. Once I’d left the corporate world, I bought my first camera to see if I was any good. I began my journey as a professional photographer by taking pictures of random scenes and people, and it was from here I fell in love with everything about this field.</p>
+        <p>I felt at home capturing art and connecting with my subjects both in person and through a lens, and I was transformed into a more sensitive, perceptive, and attuned person because of it.</p>
+        <p>My imagination is also sparked by motherhood, romance, and perfect pairings of angles and light. My favourite memories to capture are intimate glances and laughs, sweet cuddles, and humble beauty.</p>
+      </div>
+      <a href="#" class="text-images__btn btn">explore portfolio</a>      
+    </div>
+    <div class="text-images__wrap">
+      <div class="text-images__img-wrapper desktop horisontal">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/text-images-desktop-01.jpg" alt="" class="text-images__img">
+      </div>
+      <div class="text-images__img-wrapper desktop vertical">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/text-images-desktop-02.jpg" alt="" class="text-images__img">
+      </div> 
+    </div>
+    </div>
+  </div>
 </section>
+
+
+
+
+
+
 
