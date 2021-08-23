dae856c7df7115b9ecaef9553ccb17cc263f311a
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-08-23 22:32:11 +0300

    add arrows to slider

--- template-parts/rows/feedback-slider-section.php
+++ template-parts/rows/feedback-slider-section.php
@@ -2,7 +2,7 @@
 
 ?>
 
-<section class="feedback">
+<section class="feedback feedback--slider">
   <div class="feedback__bg"></div>
 <!--  <div class="feedback__content-bg"></div>-->
   <div class="feedback__center center">
@@ -10,6 +10,22 @@
       <div class="swiper-container">
         <div class="swiper-wrapper">
 
+          
+          <div class="swiper-slide">
+            <div class="feedback__quotemark">
+              <svg class="feedback__quotemark-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 17">
+                <path d="M12,6.18A6.09,6.09,0,0,0,6,0C2.71,0,.31,2.67,0,6.08c-.42,5.19,3.39,9.54,8,10.92a6.22,6.22,0,0,1-2.87-4.7,5.57,5.57,0,0,0,.83.07,6.09,6.09,0,0,0,6-6.19Z"/>
+                <path d="M25,6.18A6.09,6.09,0,0,0,19,0c-3.31,0-5.71,2.67-6,6.08-.42,5.19,3.39,9.54,8,10.92a6.22,6.22,0,0,1-2.87-4.7,5.69,5.69,0,0,0,.83.07,6.09,6.09,0,0,0,6-6.19Z"/>
+              </svg>
+            </div>
+            <div class="feedback__quote wysiwyg-content">
+              <p>A wedding, pre-wedding and fashion photographer based in central Europe, I specialise in telling visual stories and shooting destination weddings. My work combines my two great loves: photography and travel.</p>
+
+              <p>There is nowhere and no lengths I won’t go to capture a wedding or engagement – the more unique the better. I’ll be wherever you are!</p>
+            </div>
+            <div class="feedback__author">- Katie & Joe</div>
+          </div>
+        
           <div class="swiper-slide">
             <div class="feedback__quotemark">
               <svg class="feedback__quotemark-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 17">
@@ -24,33 +40,29 @@
             </div>
             <div class="feedback__author">- Frej & Chinami</div>
           </div>
+        </div>
 
-          <div class="swiper-slide">
-            <div class="feedback__quotemark">
-              <svg class="feedback__quotemark-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 17">
-                <path d="M12,6.18A6.09,6.09,0,0,0,6,0C2.71,0,.31,2.67,0,6.08c-.42,5.19,3.39,9.54,8,10.92a6.22,6.22,0,0,1-2.87-4.7,5.57,5.57,0,0,0,.83.07,6.09,6.09,0,0,0,6-6.19Z"/>
-                <path d="M25,6.18A6.09,6.09,0,0,0,19,0c-3.31,0-5.71,2.67-6,6.08-.42,5.19,3.39,9.54,8,10.92a6.22,6.22,0,0,1-2.87-4.7,5.69,5.69,0,0,0,.83.07,6.09,6.09,0,0,0,6-6.19Z"/>
-              </svg>
-            </div>
-            <div class="feedback__quote wysiwyg-content">
-              <p>We were looking for a photographer that could deliver personal photographies with the best quality of our wedding day and we’re happy to say that Dina Deykun exceeded our expectations. She had a great sense of making our pictures  unique and improvised some amazing shots that we are now lucky to have for the rest of our lives.</p>
 
-              <p>We really recommend Dina to everyone who wants beautiful and unique pictures taken of their special day. Thank you Dina!</p>
-            </div>
-            <div class="feedback__author">- Frej & Chinami</div>
+        <div class="feedback__slider-bottom">
+          <div class="swiper-button-prev">
+            <svg width="56" height="7" viewBox="0 0 56 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M55.5 4H56V3H55.5V4ZM0.5 3.5L5.5 6.38675V0.613249L0.5 3.5ZM55.5 3H5V4H55.5V3Z" fill="white"/>
+            </svg>
           </div>
 
+          <div class="feedback__numbers">
+            <span class="feedback__number current">1</span>
+            <span class="feedback__slash"></span>
+            <span class="feedback__number total">5</span>
+          </div>
+
+          <div class="swiper-button-next">
+            <svg width="57" height="7" viewBox="0 0 57 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0.5 3H0V4H0.5V3ZM56.5 3.5L51.5 0.613249V6.38675L56.5 3.5ZM0.5 4H52V3H0.5V4Z" fill="white"/>
+            </svg>
+          </div>
         </div>
 
-        <div class="swiper-button-prev"></div>
-
-        <div class="hero-slider__numbers">
-          <span class="hero-slider__number current">1</span>
-          <span class="hero-slider__slash"></span>
-          <span class="hero-slider__number total">5</span>
-        </div>
-
-        <div class="swiper-button-next"></div>
 
       </div>
     <!-- </div> -->
