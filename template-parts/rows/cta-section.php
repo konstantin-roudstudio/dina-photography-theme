d4ad18c22e073806310d126186d008c87b6fbb3d
GennadiiRoud <72335453+GennadiiRoud@users.noreply.github.com>
2021-08-23 12:59:52 +0300

    cta-section changes

--- template-parts/rows/cta-section.php
+++ template-parts/rows/cta-section.php
@@ -1,7 +1,31 @@
 <?php
 ?>
 
-<section class="cta-section">
-  CTA SECTION
+<section class="cta-section cta-section--cream">
+	<div class="center">
+		<div class="cta-section__inner">
+			<div class="cta-section__img-wrapper desktop">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/cta-section-img.jpg" alt="" class="cta-section__img">
+			</div>
+			<div class="cta-section__content">
+				<h2 class="cta-section__title">Every Story is Unique</h2>
+				<div class="cta-section__img-wrapper mob">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/cta-section-img.jpg" alt="" class="cta-section__img">
+				</div>
+				<div class="cta-section__text">I hope you’ve resonated with my story about who I am and how I got here. I’m
+					based wherever my work takes me, and I am someone that relishes in telling other’s stories. I look forward to
+					hearing from you and discovering the perfect way to document your legacy.</div>
+				<div class="cta-section__link-wrapper">
+					<a href="#" class="cta-section__btn btn btn--calendar">
+						<svg class="btn__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+							<path
+								d="M12.09,1.45H10.82V.61A.63.63,0,0,0,10.18,0a.62.62,0,0,0-.63.61v.84H4.45V.61A.62.62,0,0,0,3.82,0a.63.63,0,0,0-.64.61v.84H1.91A1.88,1.88,0,0,0,0,3.29v8.87A1.87,1.87,0,0,0,1.91,14H12.09A1.87,1.87,0,0,0,14,12.16V3.29A1.88,1.88,0,0,0,12.09,1.45Zm.64,10.71a.61.61,0,0,1-.19.44.67.67,0,0,1-.45.18H1.91a.63.63,0,0,1-.64-.62v-5H12.73Zm0-6.27H1.27V3.29a.63.63,0,0,1,.64-.61H3.18v.69A.63.63,0,0,0,3.82,4a.62.62,0,0,0,.63-.61V2.68h5.1v.69a.62.62,0,0,0,.63.61.63.63,0,0,0,.64-.61V2.68h1.27a.63.63,0,0,1,.45.18.59.59,0,0,1,.19.43Z" />
+						</svg>
+						check your date
+					</a>
+					<a href="#" class="cta-section__link">explore portfolio</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
-
