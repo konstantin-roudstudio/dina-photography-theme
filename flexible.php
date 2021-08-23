4d08be01aaba36f5f68bff30536c0767a7125dee
konstantin-roudstudio <konstantin@roudstudio.com>
2021-08-23 10:04:04 +0300

    preparations

--- flexible.php
+++ flexible.php
@@ -5,16 +5,25 @@
 
 get_header();
 ?>
-    <main id="main" class="main">
-        <?php
-        if (have_rows('page_builder')):
-            while (have_rows('page_builder')): the_row();
-                if (get_row_layout() == 'hero_info_section'): get_template_part('template-parts/rows/hero-info-section', 'section');
-                endif;
-            endwhile;
+  <main id="main" class="main">
+    <?php
+    if (have_rows('page_builder')):
+      while (have_rows('page_builder')): the_row();
+        if (get_row_layout() == 'hero_info_section'): get_template_part('template-parts/rows/hero-info-section', 'section');
+        elseif (get_row_layout() == 'about_info_section'): get_template_part('template-parts/rows/about-info-section', 'section');
+        elseif (get_row_layout() == 'text_images_section'): get_template_part('template-parts/rows/text-images-section', 'section');
+        elseif (get_row_layout() == 'cta_section'):
+          get_template_part('template-parts/rows/cta-section', 'section');
+//                  if (get_sub_field('override')) :
+//                    get_template_part('template-parts/rows/cta-section', 'section');
+//                  else  :
+//                    get_template_part('template-parts/global-rows/cta-section', 'section');
+//                  endif;
         endif;
-        ?>
-    </main><!-- #main -->
+      endwhile;
+    endif;
+    ?>
+  </main><!-- #main -->
 
 <?php
 get_footer();
