5da03976469dba2e365238826417c07167cfe339
konstantin-roudstudio <konstantin@roudstudio.com>
2021-08-23 13:30:32 +0300

    preparations

--- flexible.php
+++ flexible.php
@@ -7,7 +7,8 @@ get_header();
 ?>
     <main id="main" class="main">
         <?php
-        get_template_part('template-parts/rows/feedback-section', 'section');
+        get_template_part('template-parts/rows/feedback-slider-section', 'section');
+//        get_template_part('template-parts/rows/feedback-section', 'section');
         if (have_rows('page_builder')):
             while (have_rows('page_builder')): the_row();
                 if (get_row_layout() == 'hero_info_section'): get_template_part('template-parts/rows/hero-info-section', 'section');
