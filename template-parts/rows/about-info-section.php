ff67f19211d80a42215d74a3dedfed87cd84600d
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-08-23 11:31:02 +0300

    about changes

--- template-parts/rows/about-info-section.php
+++ template-parts/rows/about-info-section.php
@@ -2,8 +2,20 @@
 ?>
 
 <section class="about">
-  <div class="about__inner">
-    ABOUT SECTION
-  </div>  
+  <div class="about__center center">
+    <div class="about__inner">
+      <div class="about__title title">I work to capture the beauty, emotion and magic of the most important day of your life. </div>
+      <div class="about__content-wrapper">
+        <div class="about__img">
+          <img src="<?php echo get_template_directory_uri(); ?> /images/about__img.jpg" alt="">
+        </div>
+        <div class="about__content">
+          <div class="about__text">My philosophy is to create understanding relationships with clients, listen to their vision, and incorporate it with my style – I have found this approach results in beautiful, authentic photographs that will become your legacy for future generations.</div>
+          <a href="#" class="about__btn btn">My Story</a>
+        </div>
+      </div>
+      <div class="about__partners"></div>
+    </div>
+  </div> 
 </section>
 
