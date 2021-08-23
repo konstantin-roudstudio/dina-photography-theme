ded7ffda2287d24a8e9580111be11dcf5a609816
konstantin-roudstudio <konstantin@roudstudio.com>
2021-08-23 13:18:29 +0300

    fixes

--- template-parts/rows/promo-section.php
+++ template-parts/rows/promo-section.php
@@ -3,8 +3,14 @@
   <div class="promo__overlay"></div>
   <div class="promo__center center">
     <div class="promo__content">
-      <h1 class="promo__title title">It’s my love for travelling and romance that set my destiny as a destination wedding photographer.</h1>
+      <h2 class="promo__title title">It’s my love for travelling and romance that set my destiny as a destination wedding photographer.</h2>
       <div class="promo__text">My ability to listen, form quick resolutions to problems, and strong-willed character make me stand out in the photography world.</div>
+<!--      <a href="#" class="promo__btn btn btn--calendar">-->
+<!--        <svg class="btn__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">-->
+<!--          <path d="M12.09,1.45H10.82V.61A.63.63,0,0,0,10.18,0a.62.62,0,0,0-.63.61v.84H4.45V.61A.62.62,0,0,0,3.82,0a.63.63,0,0,0-.64.61v.84H1.91A1.88,1.88,0,0,0,0,3.29v8.87A1.87,1.87,0,0,0,1.91,14H12.09A1.87,1.87,0,0,0,14,12.16V3.29A1.88,1.88,0,0,0,12.09,1.45Zm.64,10.71a.61.61,0,0,1-.19.44.67.67,0,0,1-.45.18H1.91a.63.63,0,0,1-.64-.62v-5H12.73Zm0-6.27H1.27V3.29a.63.63,0,0,1,.64-.61H3.18v.69A.63.63,0,0,0,3.82,4a.62.62,0,0,0,.63-.61V2.68h5.1v.69a.62.62,0,0,0,.63.61.63.63,0,0,0,.64-.61V2.68h1.27a.63.63,0,0,1,.45.18.59.59,0,0,1,.19.43Z"/>-->
+<!--        </svg>-->
+<!--        <span>check your date</span>-->
+<!--      </a>-->
     </div>
-  </div>   
+  </div>
 </section>
