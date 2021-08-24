38ea449b785cc28654901f7a8bfcabe9109aaa35
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-08-24 20:26:04 +0300

    add desktop stylies

--- assets_src/js/modules/portfolio-slider.js
+++ assets_src/js/modules/portfolio-slider.js
@@ -6,6 +6,11 @@ export default function portfolioSlider() {
 
   const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
+    breakpoints: {
+        767: {
+          slidesPerView: 2,
+        },
+      },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
