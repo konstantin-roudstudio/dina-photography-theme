83f0d26d4a8cd4b1b46d2f430fd857227bb05c86
GennadiiRoud <72335453+GennadiiRoud@users.noreply.github.com>
2021-08-24 13:26:01 +0300

    text-boxes changes

--- template-parts/rows/text-boxes-section.php
+++ template-parts/rows/text-boxes-section.php
@@ -1,3 +1,68 @@
-<section class="text-boxes">
-  TEXT BOXES
+<section class="text-boxes text-boxes--cream text-boxes--inverted">
+	<div class="center">
+		<div class="text-boxes__inner">
+			<div class="text-boxes__head">
+				<h2 class="text-boxes__title">Where in France?</h2>
+				<a href="#" class="text-boxes__btn btn btn--calendar">
+					<svg class="btn__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">-->
+						<path d="M12.09,1.45H10.82V.61A.63.63,0,0,0,10.18,0a.62.62,0,0,0-.63.61v.84H4.45V.61A.62.62,0,0,0,3.82,0a.63.63,0,0,0-.64.61v.84H1.91A1.88,1.88,0,0,0,0,3.29v8.87A1.87,1.87,0,0,0,1.91,14H12.09A1.87,1.87,0,0,0,14,12.16V3.29A1.88,1.88,0,0,0,12.09,1.45Zm.64,10.71a.61.61,0,0,1-.19.44.67.67,0,0,1-.45.18H1.91a.63.63,0,0,1-.64-.62v-5H12.73Zm0-6.27H1.27V3.29a.63.63,0,0,1,.64-.61H3.18v.69A.63.63,0,0,0,3.82,4a.62.62,0,0,0,.63-.61V2.68h5.1v.69a.62.62,0,0,0,.63.61.63.63,0,0,0,.64-.61V2.68h1.27a.63.63,0,0,1,.45.18.59.59,0,0,1,.19.43Z" />
+					</svg>
+					<span>check your date</span>
+				</a>
+			</div>
+
+			<div class="text-boxes__items">
+				<div class="text-boxes__item">
+					<h3 class="text-boxes__item-title">Paris</h3>
+					<div class="text-boxes__item-text">
+            <p>A fashion capital of the world and the city of love. If you’re looking to
+						make a statement with your wedding, Paris is a classic. Featuring iconic landmarks around every corner,
+						glamorous hotels, and luxurious venues, Paris is a popular choice for A-listers and hopeless romantics
+						alike. Getting married in the city of love is a dream come true, and a pleasure to snap for any
+						photographers in France.</p>
+          </div>
+					<div class="text-boxes__img-wrapper">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/text-boxes-01.jpg" alt="" class="text-boxes__img">
+					</div>
+					<a href="#" class="text-boxes__link">explore more</a>
+				</div>
+				<div class="text-boxes__item">
+					<h3 class="text-boxes__item-title">Provence</h3>
+					<div class="text-boxes__item-text">
+            <p>The Provence region is located in the southeast of France, along the Mediterranean coastline. It is known for its beautiful rolling hills, charming countryside, and vineyards. For wedding photographers, Provence is an ideal location for rustic photos. There are many historic chateaus and luxurious hotels in Provence that can serve as the perfect wedding venue for your big day.</p> 
+          </div>
+					<div class="text-boxes__img-wrapper">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/text-boxes-02.jpg" alt="" class="text-boxes__img">
+					</div>
+					<a href="#" class="text-boxes__link">explore more</a>
+				</div>
+				<div class="text-boxes__item">
+					<h3 class="text-boxes__item-title">South of France</h3>
+					<div class="text-boxes__item-text">
+            <p></p>     
+          </div>
+					<div class="text-boxes__img-wrapper">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/text-boxes-03.jpg" alt="" class="text-boxes__img">
+					</div>
+					<a href="#" class="text-boxes__link">explore more</a>
+				</div>
+				<div class="text-boxes__item">
+					<h3 class="text-boxes__item-title">South of France</h3>
+					<div class="text-boxes__item-text">
+            <p></p>      
+          </div>
+					<div class="text-boxes__img-wrapper">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/text-boxes-03.jpg" alt="" class="text-boxes__img">
+					</div>
+					<a href="#" class="text-boxes__link">explore more</a>
+				</div>
+			</div>
+			<a href="#" class="text-boxes__btn btn btn--calendar">
+				<svg class="btn__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">-->
+					<path d="M12.09,1.45H10.82V.61A.63.63,0,0,0,10.18,0a.62.62,0,0,0-.63.61v.84H4.45V.61A.62.62,0,0,0,3.82,0a.63.63,0,0,0-.64.61v.84H1.91A1.88,1.88,0,0,0,0,3.29v8.87A1.87,1.87,0,0,0,1.91,14H12.09A1.87,1.87,0,0,0,14,12.16V3.29A1.88,1.88,0,0,0,12.09,1.45Zm.64,10.71a.61.61,0,0,1-.19.44.67.67,0,0,1-.45.18H1.91a.63.63,0,0,1-.64-.62v-5H12.73Zm0-6.27H1.27V3.29a.63.63,0,0,1,.64-.61H3.18v.69A.63.63,0,0,0,3.82,4a.62.62,0,0,0,.63-.61V2.68h5.1v.69a.62.62,0,0,0,.63.61.63.63,0,0,0,.64-.61V2.68h1.27a.63.63,0,0,1,.45.18.59.59,0,0,1,.19.43Z" />
+				</svg>
+				<span>check your date</span>
+			</a>
+		</div>
+	</div>
 </section>
