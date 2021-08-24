2fff056590b8458eb585628cd81661664dc73000
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-08-24 14:09:14 +0300

    add portfolio-slider section

--- template-parts/rows/portfolio-slider-section.php
+++ template-parts/rows/portfolio-slider-section.php
@@ -3,6 +3,14 @@
 ?>
 
 <section class="portfolio-slider">
-  PORTFOLIO SLIDER
+  <div class="portfolio-slider__center center">
+    <div class="portfolio-slider__inner">
+      <div class="portfolio-slider__content">
+        <div class="portfolio-slider__title title">Recent Weddings</div>
+        <div class="portfolio-slider__text">As your wedding photographer, my mission is to create beautiful, timeless, and natural photographs that you can cherish forever.</div>
+        <a href="#" class="portfolio-slider__btn btn">explore portfolio</a>
+      </div>
+    </div>
+  </div>  
 </section>
 
