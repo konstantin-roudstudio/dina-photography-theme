d817a2ebbda0760f8f6d2caf1203bb388a01b08b
konstantin-roudstudio <konstantin@roudstudio.com>
2021-08-25 10:44:42 +0300

    preparations

--- flexible.php
+++ flexible.php
@@ -7,10 +7,10 @@ get_header();
 ?>
     <main id="main" class="main">
         <?php
-        get_template_part('template-parts/rows/feedback-section', 'section');
         if (have_rows('page_builder')):
             while (have_rows('page_builder')): the_row();
                 if (get_row_layout() == 'hero_info_section'): get_template_part('template-parts/rows/hero-info-section', 'section');
+                elseif (get_row_layout() == 'feedback_section'): get_template_part('template-parts/rows/feedback-section', 'section');
                 endif;
             endwhile;
         endif;
--- template-parts/rows/feedback-section.php
+++ template-parts/rows/feedback-section.php
@@ -1,26 +1,31 @@
 <?php
-
+$review_post = get_field('review_post');
+var_dump($review_post);
+$text = get_field('text', $review_post->ID);
+$img = get_field('image', $review_post->ID);
+var_dump($text);
+var_dump($img);
 ?>
 
 <section class="feedback">
-  <div class="feedback__bg"></div>
-<!--  <div class="feedback__content-bg"></div>-->
+  <?php if ($img) : ?>
+    <div class="feedback__bg"><?= wp_get_attachment_image($img, 'full') ?></div>
+  <?php endif; ?>
+
   <div class="feedback__center center">
     <div class="feedback__content">
-
-        <div class="feedback__quotemark">
-          <svg class="feedback__quotemark-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 17">
-            <path d="M12,6.18A6.09,6.09,0,0,0,6,0C2.71,0,.31,2.67,0,6.08c-.42,5.19,3.39,9.54,8,10.92a6.22,6.22,0,0,1-2.87-4.7,5.57,5.57,0,0,0,.83.07,6.09,6.09,0,0,0,6-6.19Z"/>
-            <path d="M25,6.18A6.09,6.09,0,0,0,19,0c-3.31,0-5.71,2.67-6,6.08-.42,5.19,3.39,9.54,8,10.92a6.22,6.22,0,0,1-2.87-4.7,5.69,5.69,0,0,0,.83.07,6.09,6.09,0,0,0,6-6.19Z"/>
-          </svg>
-        </div>
-        <div class="feedback__quote wysiwyg-content">
-          <p>We were looking for a photographer that could deliver personal photographies with the best quality of our wedding day and we’re happy to say that Dina Deykun exceeded our expectations. She had a great sense of making our pictures  unique and improvised some amazing shots that we are now lucky to have for the rest of our lives.</p>
-
-          <p>We really recommend Dina to everyone who wants beautiful and unique pictures taken of their special day. Thank you Dina!</p>
-        </div>
-        <div class="feedback__author">- Frej & Chinami</div>
-
+      <div class="feedback__quotemark">
+        <svg class="feedback__quotemark-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 17">
+          <path
+              d="M12,6.18A6.09,6.09,0,0,0,6,0C2.71,0,.31,2.67,0,6.08c-.42,5.19,3.39,9.54,8,10.92a6.22,6.22,0,0,1-2.87-4.7,5.57,5.57,0,0,0,.83.07,6.09,6.09,0,0,0,6-6.19Z"/>
+          <path
+              d="M25,6.18A6.09,6.09,0,0,0,19,0c-3.31,0-5.71,2.67-6,6.08-.42,5.19,3.39,9.54,8,10.92a6.22,6.22,0,0,1-2.87-4.7,5.69,5.69,0,0,0,.83.07,6.09,6.09,0,0,0,6-6.19Z"/>
+        </svg>
+      </div>
+      <div class="feedback__quote wysiwyg-content">
+        <?= $text ?>
+      </div>
+      <div class="feedback__author">- <?= esc_html($review_post->post_title); ?></div>
     </div>
   </div>
 </section>
