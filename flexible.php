80b029e79ae20374c9c41e40557a045564201b52
konstantin-roudstudio <konstantin@roudstudio.com>
2021-08-26 11:40:31 +0300

    fixes

--- flexible.php
+++ flexible.php
@@ -10,7 +10,6 @@ get_header();
     if (have_rows('page_builder')):
       while (have_rows('page_builder')): the_row();
         if (get_row_layout() == 'about_info_section'): get_template_part('template-parts/rows/about-info-section', 'section');
-        elseif (get_row_layout() == 'cta_section'):
         elseif (get_row_layout() == 'feedback_section'): get_template_part('template-parts/rows/feedback-section', 'section');
         elseif (get_row_layout() == 'hero_info_section'): get_template_part('template-parts/rows/hero-info-section', 'section');
         elseif (get_row_layout() == 'hero_reviews_section'): get_template_part('template-parts/rows/hero-reviews-section', 'section');
@@ -18,6 +17,7 @@ get_header();
         elseif (get_row_layout() == 'hero_about_section'): get_template_part('template-parts/rows/hero-about-section', 'section');
         elseif (get_row_layout() == 'text_images_section'): get_template_part('template-parts/rows/text-images-section', 'section');
         elseif (get_row_layout() == 'cta_form_section'): get_template_part('template-parts/rows/cta-form-section', 'section');
+        elseif (get_row_layout() == 'cta_section'):
           get_template_part('template-parts/rows/cta-section', 'section');
 //                  if (get_sub_field('override')) :
 //                    get_template_part('template-parts/rows/cta-section', 'section');
