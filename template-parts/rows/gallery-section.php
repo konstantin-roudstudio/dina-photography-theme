e252c5a8196541238596b8540c356468831f9906
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-08-27 17:02:34 +0300

    gallery changes

--- template-parts/rows/gallery-section.php
+++ template-parts/rows/gallery-section.php
@@ -1,17 +1,81 @@
 <section class="gallery">
-  <div class="gallery__row center">
-    <div class="gallery__item gallery__item--half">
-      <div class="gallery__item-wrapper" style="padding-bottom:66.6%;"><img src="" alt=""></div>
+  <div class="gallery__inner">
+    <div class="gallery__content center">
+      <div class="gallery__title title">porfolio gallery</div>
+      <div class="gallery__text">
+        <p>To stand out as a wedding photographer can be very hard. I try to make my wedding photography style unique and creative for each couple. I love to make a reportage of the wedding day as well as implement magazine style in my photography work.</p>
+        <p>Here is a quick overview of my wedding photography style!</p>
+      </div>
     </div>
-  </div>
+    <div class="gallery__row">
+      <div class="gallery__item">
+        <div class="gallery__item-wrapper" style="padding-bottom:55.5%;"><img src="" alt=""></div>
+      </div>
+    </div>
 
-  <div class="gallery__row center">
-    <div class="gallery__item gallery__item--half">
-      <div class="gallery__item-wrapper" style="padding-bottom:150%;"><img src="" alt=""></div>
+    <div class="gallery__row center">
+      <div class="gallery__item gallery__item--half">
+        <div class="gallery__item-wrapper" style="padding-bottom:150%;"><img src="" alt=""></div>
+      </div>
+
+      <div class="gallery__item gallery__item--half">
+        <div class="gallery__item-wrapper" style="padding-bottom:150%;"><img src="" alt=""></div>
+      </div>
     </div>
 
-    <div class="gallery__item gallery__item--half">
-      <div class="gallery__item-wrapper" style="padding-bottom:150%;"><img src="" alt=""></div>
+    <div class="gallery__row center">
+      <div class="gallery__item">
+        <div class="gallery__item-wrapper" style="padding-bottom:66.6%;"><img src="" alt=""></div>
+      </div>
     </div>
+
+    <div class="gallery__row center">
+      <div class="gallery__item gallery__item--half">
+        <div class="gallery__item-wrapper" style="padding-bottom:66.6%;"><img src="" alt=""></div>
+      </div>
+
+      <div class="gallery__item gallery__item--half">
+        <div class="gallery__item-wrapper" style="padding-bottom:66.6%;"><img src="" alt=""></div>
+      </div>
+    </div>
+
+    <div class="gallery__row center">
+      <div class="gallery__item gallery__item--third">
+        <div class="gallery__item-wrapper" style="padding-bottom:150%;"><img src="" alt=""></div>
+      </div>
+
+      <div class="gallery__item gallery__item--third">
+        <div class="gallery__item-wrapper" style="padding-bottom:150%;"><img src="" alt=""></div>
+      </div>
+
+      <div class="gallery__item gallery__item--third">
+        <div class="gallery__item-wrapper" style="padding-bottom:150%;"><img src="" alt=""></div>
+      </div>
+    </div>
+
+    <div class="gallery__row center">
+      <div class="gallery__item gallery__item--two-thirds">
+        <div class="gallery__item-wrapper" style="padding-bottom:71.2%;"><img src="" alt=""></div>
+      </div>
+
+      <div class="gallery__item gallery__item--third">
+        <div class="gallery__item-wrapper" style="padding-bottom:150%;"><img src="" alt=""></div>
+      </div>
+    </div>
+
+    <div class="gallery__row center">
+      <div class="gallery__item gallery__item--third">
+        <div class="gallery__item-wrapper" style="padding-bottom:66.6%;"><img src="" alt=""></div>
+      </div>
+
+      <div class="gallery__item gallery__item--third">
+        <div class="gallery__item-wrapper" style="padding-bottom:66.6%;"><img src="" alt=""></div>
+      </div>
+
+      <div class="gallery__item gallery__item--third">
+        <div class="gallery__item-wrapper" style="padding-bottom:66.6%;"><img src="" alt=""></div>
+      </div>
+    </div>
+
   </div>
 </section>
