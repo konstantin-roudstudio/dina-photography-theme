639e548f6e7b68576deb2ee6a39ebeff6eb767b6
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-08-30 14:06:58 +0300

    add blog listing

--- page-blog-archive.php
+++ page-blog-archive.php
@@ -12,10 +12,142 @@ get_header();
 
       <div class="blog-listing">
         <div class="center blog-listing__center">
-          <h1 class="blog-listing__title title">TITLE</h1>
+          <h1 class="blog-listing__title title">Journal. Journey. And much more.</h1>
 
           <div class="blog-listing__items">
-            BLOG ITEMS HERE!
+            
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">What is Editorial Photography?</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>           
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">Hiring a Photographer for Your Destination Wedding</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">How to calculate a budget for a wedding photographer?</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">What is Editorial Photography?</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>           
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">Hiring a Photographer for Your Destination Wedding</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">How to calculate a budget for a wedding photographer?</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">What is Editorial Photography?</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>           
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">Hiring a Photographer for Your Destination Wedding</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">How to calculate a budget for a wedding photographer?</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>
+            </div>  
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">What is Editorial Photography?</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>           
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">Hiring a Photographer for Your Destination Wedding</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>
+            </div>
+
+            <div class="blog-listing__item">
+              <div class="blog-listing__date">18 Aug 2021</div>
+              <div class="blog-listing__item-title">How to calculate a budget for a wedding photographer?</div>
+              <div class="blog-listing__content-wrapper">
+                <div class="blog-listing__img-wrapper">
+                  <img src="" alt="">
+                </div>
+                <a href="#" class="blog-listing__link">read more</a>
+              </div>
+            </div>         
+
           </div>
         </div>
       </div>
