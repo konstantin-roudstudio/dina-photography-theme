c22522baf94d7cfc484026abf2eef8c60e021926
konstantin-roudstudio <konstantin@roudstudio.com>
2021-08-30 12:03:04 +0300

    preparations

---
+++ page-blog-archive.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Blog Archive
+*/
+
+get_header();
+?>
+
+  <div id="primary" class="site-content">
+    <div id="content" role="main">
+
+
+      <div class="blog-listing">
+        <div class="center blog-listing__center">
+          <h1 class="blog-listing__title title">TITLE</h1>
+
+          <div class="blog-listing__items">
+            BLOG ITEMS HERE!
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php get_footer(); ?>
