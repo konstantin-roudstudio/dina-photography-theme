7fa1928bc5d431156e4ce0f72a194abb869f7c13
konstantin-roudstudio <konstantin@roudstudio.com>
2021-09-02 11:32:11 +0300

    preparations

--- single.php
+++ single.php
@@ -17,46 +17,48 @@ $archive_link = get_post_type_archive_link( $type );
 ?>
   <main id="primary" class="main">
 
-    <?php
-    while (have_posts()) :
-      the_post();
-      ?>
+<!--    --><?php
+//    while (have_posts()) :
+//      the_post();
+//      ?>
+<!---->
+<!--      <article class="post">-->
+<!--        <div class="center post__center">-->
+<!--          <h1 class="post__title title-xl">-->
+<!--            --><?php //the_title(); ?>
+<!--          </h1>-->
+<!---->
+<!--          <div class="post__image">-->
+<!--            --><?//= $thumbnail ?>
+<!--          </div>-->
+<!---->
+<!--          <div class="post__content wysiwyg-content">-->
+<!--            --><?php
+//            the_content(
+//                sprintf(
+//                    wp_kses(
+//                    /* translators: %s: Name of current post. Only visible to screen readers */
+//                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'dina-photography'),
+//                        array(
+//                            'span' => array(
+//                                'class' => array(),
+//                            ),
+//                        )
+//                    ),
+//                    wp_kses_post(get_the_title())
+//                )
+//            );
+//            ?>
+<!--          </div>-->
+<!--      </article>-->
+<!--    --><?php
+//    endwhile;
+//    ?>
 
-      <article class="post">
-        <div class="center post__center">
-          <h1 class="post__title title-xl">
-            <?php the_title(); ?>
-          </h1>
-
-          <div class="post__image">
-            <?= $thumbnail ?>
-          </div>
-
-          <div class="post__content wysiwyg-content">
-            <?php
-            the_content(
-                sprintf(
-                    wp_kses(
-                    /* translators: %s: Name of current post. Only visible to screen readers */
-                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'dina-photography'),
-                        array(
-                            'span' => array(
-                                'class' => array(),
-                            ),
-                        )
-                    ),
-                    wp_kses_post(get_the_title())
-                )
-            );
-            ?>
-          </div>
-      </article>
-    <?php
-    endwhile; // End of the loop.
-    ?>
+    ADD SECTIONS HERE
 
   </main><!-- #main -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
