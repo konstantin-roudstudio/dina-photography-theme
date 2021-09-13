ae4d32a8e2ea26a47731872295b9a84c255a2fbe
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-09-13 19:00:03 +0300

    add contact (not ready)

--- template-parts/rows/contact-section.php
+++ template-parts/rows/contact-section.php
@@ -10,6 +10,16 @@ $shortcode = get_sub_field('form');
   <div class="contact__content">
     <h1 class="title contact__title"><?= $title ?></h1>
     <div class="contact__text"><?= $text ?></div>
+    <div class="contact__content-row">
+      <div class="contact__content-item">
+        <div class="contact__content-title">email me</div>
+        <div class="contact__content-text">di@dinadeykun.com</div>
+      </div>
+      <div class="contact__content-item">
+        <div class="contact__content-title">call me</div>
+        <div class="contact__content-text">+48 784 772 391</div>
+      </div>
+    </div>
     <div class="contact__form"><?= do_shortcode($shortcode); ?></div>
   </div>
 </section>
