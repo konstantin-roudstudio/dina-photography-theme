d5ba1118db4147e0b8906c26e935ac48a385215e
konstantin-roudstudio <konstantin@roudstudio.com>
2021-09-14 13:57:02 +0300

    php fixes

--- template-parts/rows/text-images-section.php
+++ template-parts/rows/text-images-section.php
@@ -1,29 +1,65 @@
 <?php
+$inverted = get_sub_field('inverted');
+$color = get_sub_field('bg_color');
+$button = get_sub_field('button');
+$check_button = get_sub_field('check_date_button');
+$title = get_sub_field('title');
+$subtitle = get_sub_field('subtitle');
+$text = get_sub_field('text');
+$note = get_sub_field('note');
+$image1 = get_sub_field('image_1');
+$image2 = get_sub_field('image_2');
+
+if ($button) {
+  $button_url = $button['url'];
+  $button_title = $button['title'];
+  $button_target = $button['target'] ? $button['target'] : '_self';
+}
 ?>
 
-<section class="text-images text-images--cream">
+<section class="text-images<?php if ($color != 'white') echo ' text-images--'.$color; ?>">
   <div class="center">
     <div class="text-images__inner">
       <div class="text-images__content">
-      <h2 class="text-images__title">A story of a girl with passion for Photography and Traveling.</h2>
+        <?php if ($title) : ?>
+          <h2 class="text-images__title"><?= $title ?></h2>
+        <?php endif; ?>
       <div class="text-images__img-wrapper mob">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/text-images-mob.jpg" alt="" class="text-images__img">
+        <?= wp_get_attachment_image($image1, 'full', null, array('class' => 'text-images__img') ) ?>
       </div>
-      <div class="text-images__text">
-        <p>Before becoming a photographer, I was working in the corporate space, specifically in the sales sector. While I was successful for many years in this work, I wasn’t happy and eventually found the courage to make a change in my professional life.</p>
-        <p>I’d always had an interest in photography. Once I’d left the corporate world, I bought my first camera to see if I was any good. I began my journey as a professional photographer by taking pictures of random scenes and people, and it was from here I fell in love with everything about this field.</p>
-        <p>I felt at home capturing art and connecting with my subjects both in person and through a lens, and I was transformed into a more sensitive, perceptive, and attuned person because of it.</p>
-        <p>My imagination is also sparked by motherhood, romance, and perfect pairings of angles and light. My favourite memories to capture are intimate glances and laughs, sweet cuddles, and humble beauty.</p>
-      </div>
-      <a href="#" class="text-images__btn btn">explore portfolio</a>      
+
+      <?php if ($text) : ?>
+        <div class="text-images__text wysiwyg-content">
+          <?= $text ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($button && !$check_button) : ?>
+        <a href="<?= $button_url ?>" target="<?= $button_target ?>" class="text-images__btn btn"><?= $button_title ?></a>
+      <?php endif; ?>
+
+      <?php if ($button && $check_button) : ?>
+
+        <a href="<?= $button_url ?>" target="<?= $button_target ?>" class="text-images__btn btn btn--calendar">
+          <svg class="btn__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+            <path d="M12.09,1.45H10.82V.61A.63.63,0,0,0,10.18,0a.62.62,0,0,0-.63.61v.84H4.45V.61A.62.62,0,0,0,3.82,0a.63.63,0,0,0-.64.61v.84H1.91A1.88,1.88,0,0,0,0,3.29v8.87A1.87,1.87,0,0,0,1.91,14H12.09A1.87,1.87,0,0,0,14,12.16V3.29A1.88,1.88,0,0,0,12.09,1.45Zm.64,10.71a.61.61,0,0,1-.19.44.67.67,0,0,1-.45.18H1.91a.63.63,0,0,1-.64-.62v-5H12.73Zm0-6.27H1.27V3.29a.63.63,0,0,1,.64-.61H3.18v.69A.63.63,0,0,0,3.82,4a.62.62,0,0,0,.63-.61V2.68h5.1v.69a.62.62,0,0,0,.63.61.63.63,0,0,0,.64-.61V2.68h1.27a.63.63,0,0,1,.45.18.59.59,0,0,1,.19.43Z"/>
+          </svg>
+          <span><?= $button_title ?></span>
+        </a>
+      <?php endif; ?>
     </div>
     <div class="text-images__wrap">
-      <div class="text-images__img-wrapper desktop horisontal">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/text-images-desktop-01.jpg" alt="" class="text-images__img">
-      </div>
-      <div class="text-images__img-wrapper desktop vertical">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/text-images-desktop-02.jpg" alt="" class="text-images__img">
-      </div> 
+      <?php if ($image1) : ?>
+        <div class="text-images__img-wrapper desktop horisontal">
+          <?= wp_get_attachment_image($image1, 'full', null, array('class' => 'text-images__img')) ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($image2) : ?>
+        <div class="text-images__img-wrapper desktop vertical">
+          <?= wp_get_attachment_image($image2, 'full', null, array('class' => 'text-images__img')) ?>
+        </div>
+      <?php endif; ?>
     </div>
     </div>
   </div>
