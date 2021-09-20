289287c786151041cbdabd2559cde31c4afb6fa1
konstantin-roudstudio <konstantin@roudstudio.com>
2021-09-20 15:21:56 +0300

    portfolio fixes

--- flexible.php
+++ flexible.php
@@ -8,9 +8,6 @@ get_header();
   <main id="main" class="main">
 
     <?php
-
-    get_template_part('template-parts/rows/portfolio-listing-section', 'section');
-
     if (have_rows('page_builder')):
       while (have_rows('page_builder')): the_row();
         if (get_row_layout() == 'about_info_section'): get_template_part('template-parts/rows/about-info-section', 'section');
--- portfolio.php
+++ portfolio.php
@@ -20,7 +20,8 @@ $landscape_items = [2, 4, 6, 8, 10, 12, 14,];
 
 $args = array(
     'post_type' => 'projects',
-    'cat' => $category_id
+    'cat' => $category_id,
+    'posts_per_page' => -1
 );
 $query = get_posts($args);
 
@@ -39,7 +40,7 @@ get_header();
 
         <div class="portfolio__inner">
           <?php foreach ($query as $index => $item) :
-            $landscape = ($index % 2 == 0) && $index != 0 ? true : false;
+            $landscape = ($index % 2 == 0) && $index != 0 && $index != 8 ? true : false;
             ?>
             <div class="portfolio__item<?php if (!$landscape) echo ' portfolio__item--portrait'; else echo ' portfolio__item--landscape'; ?>">
               <div class="portfolio__img-wrapper">
@@ -74,7 +75,12 @@ get_header();
 
               <?php if ($button) : ?>
                 <a href="<?= $button_url ?>" target="<?= $button_target ?>"
-                   class="portfolio__item-btn btn btn--calendar"><?= $button_title ?></a>
+                   class="portfolio__item-btn btn btn--calendar">
+                  <svg class="btn__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14">
+                    <path d="M12.09,1.45H10.82V.61A.63.63,0,0,0,10.18,0a.62.62,0,0,0-.63.61v.84H4.45V.61A.62.62,0,0,0,3.82,0a.63.63,0,0,0-.64.61v.84H1.91A1.88,1.88,0,0,0,0,3.29v8.87A1.87,1.87,0,0,0,1.91,14H12.09A1.87,1.87,0,0,0,14,12.16V3.29A1.88,1.88,0,0,0,12.09,1.45Zm.64,10.71a.61.61,0,0,1-.19.44.67.67,0,0,1-.45.18H1.91a.63.63,0,0,1-.64-.62v-5H12.73Zm0-6.27H1.27V3.29a.63.63,0,0,1,.64-.61H3.18v.69A.63.63,0,0,0,3.82,4a.62.62,0,0,0,.63-.61V2.68h5.1v.69a.62.62,0,0,0,.63.61.63.63,0,0,0,.64-.61V2.68h1.27a.63.63,0,0,1,.45.18.59.59,0,0,1,.19.43Z"/>
+                  </svg>
+                  <span><?= $button_title ?></span>
+                </a>
               <?php endif; ?>
             </div>
           <?php endif; ?>
