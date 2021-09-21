c5faf24f60c3aabc12890005ee54daf1313d1019
konstantin-roudstudio <konstantin@roudstudio.com>
2021-09-21 21:14:58 +0300

    fixes

--- assets_src/js/modules/mob-menu.js
+++ assets_src/js/modules/mob-menu.js
@@ -1,60 +1,71 @@
-import { disablePageScroll, enableScroll } from '../utils/scroll';
+import {disablePageScroll, enableScroll} from '../utils/scroll';
 
 export default function mobMenu($) {
-  const $wrapper = $('.wrapper');
-  const $btn = $('.menu-btn');
-  const $mobMenu = $('.main-nav');
-  const $hasChildren = $('.menu-item-has-children');
-  let windowWidth = window.innerWidth;
+    const $wrapper = $('.wrapper');
+    const $btn = $('.menu-btn');
+    const $mobMenu = $('.main-nav');
+    const $hasChildren = $('.menu-item-has-children');
+    let windowWidth = window.innerWidth;
 
-  $btn.click(function(event) {
-    event.preventDefault();
-    // $btn.toggleClass('active');
-    $wrapper.toggleClass('menu-shown');
+    $btn.click(function (event) {
+        event.preventDefault();
+        // $btn.toggleClass('active');
+        $wrapper.toggleClass('menu-shown');
 
-    if ($wrapper.hasClass('menu-shown')) {
-      disablePageScroll(true);
-    } else {
-      enableScroll();
+        if ($wrapper.hasClass('menu-shown')) {
+            disablePageScroll(true);
+        } else {
+            enableScroll();
 
-      setTimeout(function () {
-        $('.sub-menu__container').hide();
-        $hasChildren.removeClass('visible');
-      }, 500)
-    }
-  });
+            setTimeout(function () {
+                $('.sub-menu__container').hide();
+                $hasChildren.removeClass('visible');
+            }, 500)
+        }
+    });
 
-  $hasChildren.each(function() {
-    const $this = $(this);
-    const $link = $this.find('> a');
+    $hasChildren.each(function () {
+        const $this = $(this);
+        const $link = $this.find('> a');
 
-    $link.click(function(event) {
-      event.preventDefault();
-      if (window.width >= 1200) return;
+        $link.click(function (event) {
+            event.preventDefault();
+            if (window.width >= 1200) return;
 
-      if ($this.hasClass('visible')) {
-        $this.find('.sub-menu__container').hide(300);
-        console.log('HIDE');
-      } else {
-        $this.find('.sub-menu__container').show(300);
-        console.log('SHOW');
-      }
-      $this.toggleClass('visible');
-    })
-  });
+            if ($this.hasClass('visible')) {
+                $this.find('.sub-menu__container').hide(300);
+                console.log('HIDE');
+            } else {
+                $this.find('.sub-menu__container').show(300);
+                console.log('SHOW');
+            }
+            $this.toggleClass('visible');
+        });
 
-  $(window).resize(function () {
-    if (windowWidth === window.innerWidth) return;
+        $link.hover(
+            function () {
+                $wrapper.addClass('drop-shown');
+            },
+            function () {
+                setTimeout(function () {
+                    $wrapper.removeClass('drop-shown');
+                }, 300)
+            }
+        )
+    });
 
-    if (window.innerWidth >= 1200) {
-      $('.sub-menu__container').show(0);
-    } else {
-      $('.sub-menu__container').hide(0);
-    }
+    $(window).resize(function () {
+        if (windowWidth === window.innerWidth) return;
 
-    windowWidth = window.innerWidth;
-  });
+        if (window.innerWidth >= 1200) {
+            $('.sub-menu__container').show(0);
+        } else {
+            $('.sub-menu__container').hide(0);
+        }
 
-  if (window.innerWidth >= 1200) return;
-  $('.sub-menu__container').hide(0);
+        windowWidth = window.innerWidth;
+    });
+
+    if (window.innerWidth >= 1200) return;
+    $('.sub-menu__container').hide(0);
 }
--- template-parts/rows/hero-about-section.php
+++ template-parts/rows/hero-about-section.php
@@ -11,7 +11,7 @@ $bg_mob_img = $bg_mob_id ? wp_get_attachment_image($bg_mob_id, 'full') : null;
 
 ?>
 
-<section class="hero-about center">
+<section class="hero-about">
   <?php if ($bg_img): ?>
     <div class="hero-about__bg<?php if ($bg_img && $bg_mob_img) echo ' hero-about__bg--desktop'; ?>">
       <?= $bg_img ?>
@@ -30,57 +30,59 @@ $bg_mob_img = $bg_mob_id ? wp_get_attachment_image($bg_mob_id, 'full') : null;
     </div>
   <?php endif; ?>
 
-  <div class="hero-about__content">
-    <?php if ($title) : ?>
-      <h1 class="hero-about__title title"><?= nl2br($title) ?></h1>
-    <?php endif; ?>
+  <div class="hero-about__center center">
+    <div class="hero-about__content">
+      <?php if ($title) : ?>
+        <h1 class="hero-about__title title"><?= nl2br($title) ?></h1>
+      <?php endif; ?>
 
-    <?php if (!empty($text_items)) : ?>
-      <?php foreach ($text_items as $item) : ?>
-        <?php if ($item['columns'] == true) : ?>
-          <div class="hero-about__block-text hero-about__block-text--columns">
-            <p class="hero-about__text"><?= $item['text_left'] ?></p>
-            <p class="hero-about__text"><?= $item['text_right'] ?></p>
-          </div>
-        <?php else: ?>
-          <div class="hero-about__block-text">
-            <p class="hero-about__text"><?= $item['text'] ?></p>
-          </div>
-        <?php endif; ?>
-      <?php endforeach; ?>
-    <?php endif; ?>
-
-    <?php if (!empty($facts)) : ?>
-      <div class="hero-about__items">
-        <div class="hero-about__items-left">
-          <?php foreach ($facts
-
-          as $index => $item) : ?>
-          <?php if ($index >= count($facts) / 2 && $index < (count($facts) / 2) + 1) : ?>
-        </div>
-        <div class="hero-about__items-right">
-          <?php endif; ?>
-          <div class="hero-about__item">
-            <div class="hero-about__item-title"><?= $item['title'] ?></div>
-            <div class="hero-about__item-text"><?= $item['text'] ?></div>
-          </div>
-          <?php endforeach; ?>
-        </div>
-      </div>
-    <?php endif; ?>
-
-    <?php if (!empty($logos)) : ?>
-      <div class="hero-about__bottom">
-        <div class="hero-about__item-title">Some of my works published at:</div>
-        <div class="hero-about__bottom-items">
-          <?php foreach ($logos as $logo) : ?>
-            <div class="hero-about__bottom-item">
-              <?= wp_get_attachment_image($logo['image'], 'large'); ?>
+      <?php if (!empty($text_items)) : ?>
+        <?php foreach ($text_items as $item) : ?>
+          <?php if ($item['columns'] == true) : ?>
+            <div class="hero-about__block-text hero-about__block-text--columns">
+              <p class="hero-about__text"><?= $item['text_left'] ?></p>
+              <p class="hero-about__text"><?= $item['text_right'] ?></p>
             </div>
-          <?php endforeach; ?>
+          <?php else: ?>
+            <div class="hero-about__block-text">
+              <p class="hero-about__text"><?= $item['text'] ?></p>
+            </div>
+          <?php endif; ?>
+        <?php endforeach; ?>
+      <?php endif; ?>
+
+      <?php if (!empty($facts)) : ?>
+        <div class="hero-about__items">
+          <div class="hero-about__items-left">
+            <?php foreach ($facts
+
+            as $index => $item) : ?>
+            <?php if ($index >= count($facts) / 2 && $index < (count($facts) / 2) + 1) : ?>
+          </div>
+          <div class="hero-about__items-right">
+            <?php endif; ?>
+            <div class="hero-about__item">
+              <div class="hero-about__item-title"><?= $item['title'] ?></div>
+              <div class="hero-about__item-text"><?= $item['text'] ?></div>
+            </div>
+            <?php endforeach; ?>
+          </div>
         </div>
-      </div>
-    <?php endif; ?>
+      <?php endif; ?>
+
+      <?php if (!empty($logos)) : ?>
+        <div class="hero-about__bottom">
+          <div class="hero-about__item-title">Some of my works published at:</div>
+          <div class="hero-about__bottom-items">
+            <?php foreach ($logos as $logo) : ?>
+              <div class="hero-about__bottom-item">
+                <?= wp_get_attachment_image($logo['image'], 'large'); ?>
+              </div>
+            <?php endforeach; ?>
+          </div>
+        </div>
+      <?php endif; ?>
+    </div>
   </div>
 </section>
 
