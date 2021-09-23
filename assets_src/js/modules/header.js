a1267520c7e38fbe5ed778a5e8c1d5a35703aff4
konstantin-roudstudio <konstantin@roudstudio.com>
2021-09-23 19:01:10 +0300

    header fixes

--- assets_src/js/modules/header.js
+++ assets_src/js/modules/header.js
@@ -6,16 +6,23 @@ export default function stickyHeader($) {
     let actualVal = window.scrollY;
     let isFixed = false;
 
-    function checkScroll(pos) {
+    function checkScroll(pos, lastPos) {
         if ($wrapper.hasClass('mobile-menu-opened')) return;
 
         if (pos > 10) {
+            if (pos > lastPos) {
+                $wrapper.removeClass('header-shown');
+            } else {
+                $wrapper.addClass('header-shown');
+            }
+
             if (isFixed) return;
             $wrapper.addClass('header-fixed');
             isFixed = true;
+
         } else if (pos <= 10) {
             if (!isFixed) return;
-            $wrapper.removeClass('header-fixed');
+            $wrapper.removeClass('header-fixed header-shown');
             isFixed = false;
         }
     }
@@ -36,10 +43,12 @@ export default function stickyHeader($) {
             wait = false;
 
             if (lastVal !== actualVal) {
-                checkScroll(actualVal);
+                checkScroll(actualVal, lastVal);
             }
         }, 200);
 
         lastVal = actualVal;
     });
+
+    checkScroll(actualVal, lastVal);
 }
