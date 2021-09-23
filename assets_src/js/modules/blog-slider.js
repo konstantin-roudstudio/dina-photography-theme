2a220295880fc76fe40a4c356b288cf847edd6ab
Igor-Zhuravlyov <73937236+Igor-Zhuravlyov@users.noreply.github.com>
2021-09-23 10:11:03 +0300

    blog fixes

--- assets_src/js/modules/blog-slider.js
+++ assets_src/js/modules/blog-slider.js
@@ -1,32 +1,31 @@
-import Swiper, {Navigation} from 'swiper';
+import Swiper, { Navigation } from 'swiper';
 
 export default function blogSlider($) {
-    const $hero = $('.blog-slider');
-    {
-        $hero.each(function () {
-            const container = $(this).find('.swiper-container')[0];
-            const $current = $(this).find('.current');
-            const $total = $(this).find('.total');
-            const $slides = $('.swiper-slide:not(.swiper-slide-duplicate)');
+  const $hero = $('.blog-slider'); {
+    $hero.each(function() {
+      const container = $(this).find('.swiper-container')[0];
+      const $current = $(this).find('.current');
+      const $total = $(this).find('.total');
+      const $slides = $('.swiper-slide:not(.swiper-slide-duplicate)');
 
-            Swiper.use([Navigation]);
+      Swiper.use([Navigation]);
 
-            const swiper = new Swiper(container, {
-                slidesPerView: 1,
-                navigation: {
-                    nextEl: ".swiper-button-next",
-                    prevEl: ".swiper-button-prev",
-                },
-                on: {
-                    init: function (sw) {
-                        $total.text($slides.length);
-                        $current.text(sw.realIndex + 1)
-                    },
-                    slideChange: function (sw) {
-                        $current.text(sw.realIndex + 1)
-                    },
-                },
-            });
-        })
-    }
+      const swiper = new Swiper(container, {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        on: {
+          init: function(sw) {
+            $total.text($slides.length);
+            $current.text(sw.realIndex + 1)
+          },
+          slideChange: function(sw) {
+            $current.text(sw.realIndex + 1)
+          },
+        },
+      });
+    })
+  }
 }
