2511bf3d34505636f6e434f0368e984a78f7b66d
konstantin-roudstudio <konstantin@roudstudio.com>
2021-11-12 12:20:22 +0200

    head

--- header.php
+++ header.php
@@ -57,6 +57,17 @@ if ($btn) {
 
   		gtag('config', 'UA-72772880-2');
 		</script>
+	<!-- Hotjar Tracking Code for https://dinadeykun.com/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:895590,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </head>
 
 <body <?php body_class(); ?>>
